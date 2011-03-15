fe3a57795a7d21911df0010b96c9c1c6ba95620b
qichunren <whyruby@gmail.com>
2011-03-15 22:04:14 +0800

    refact product category html

--- public/company/application/views/layouts/application.php
+++ public/company/application/views/layouts/application.php
@@ -2,8 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN" dir="ltr">
   <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <title>
-    <?php if(!isset($page_title)){ echo "上海佳诚工程设备有限公司";}else{ echo $page_title;} ?></title>
+    <title><?php if(!isset($page_title)){ echo "上海佳诚工程设备有限公司";}else{ echo $page_title;} ?></title>
     <meta name="Keywords" content=" 上海佳诚工程设备有限公司、中央空调、商用空调、家用空调、冷库、制冷设备、化工制冷、医疗制冷、工业冷水、制冷机组、冷水机组、地暖、地源热泵、地源中央空调、空气源空调、压缩机、冷冻机油、制冷剂" />
     <meta name="Description" content="上海佳诚工程设备有限公司建于1996年，位于上海市普陀区金沙江路1340弄2号，本公司是一家专业从事各种制冷设备的生产、安装、销售、售后服务为一体的中大型企业。 本公司拥有一支技艺精良的技修队伍和雄厚的经济实力，曾获国家级“先进企业”荣誉称号。公司产品繁多，规格齐全 " />
 
--- public/company/application/views/products/index_view.php
+++ public/company/application/views/products/index_view.php
@@ -1,35 +1,38 @@
-<div id="sidebar">
-  <div id="product_categories">
-  <h3>产品分类</h3>
-  <ul class="categories">
+<div id="sidebar">     
+
+    <div id="product_search" style="margin-bottom:10px;">
+      <h3>产品搜索</h3>
+      <form id="search_form" method="get"><input id="keywords" type="text" name="keywords" /><button type="submit">搜</button></form>
+    </div> 
+    <script type="text/javascript">
+      $(function(){
+          $("#search_form").submit(function(){    
+              window.location.href="products/search/" + $("#keywords").val();
+              return false;
+          });
+      });
+    </script>
+    
+  <div id="product_categories" style="border:2px solid #95CFEF">
+  <h3 style="background:#95CFEF;color:#FFF;padding:4px;">产品分类</h3>
+  <div class="categories">
   <?php foreach($categories as $category): ?>
-    <li>
-      <div class="category">
-       <?php echo anchor("/products/category/".$category->id, strip_tags($category->name)); ?>
+    <dl style="border-bottom:1px dashed #BDBDBD;padding:5px;">
+      <dt class="category" style="font-weight:bold;">
+       <?php echo anchor("/products/category/".$category->id, strip_tags($category->name)); ?> 
+       </dt>
     <?php $sub_categories = $this->product->get_child_category($category->id); ?>
+      <dd class="sub_category">
       <?php foreach($sub_categories as $sub_category): ?>
-          <div class="sub_category">
-              <?php echo anchor("/products/category/".$category->id."/".$sub_category->id, strip_tags($sub_category->name)); ?>
-              </div>
-      <?php endforeach; ?>    
-      </div>        
-    </li>
+         <?php echo anchor("/products/category/".$category->id."/".$sub_category->id, strip_tags($sub_category->name)); ?> - 
+      <?php endforeach; ?> 
+      </dd>      
+    </dl>
   <?php endforeach; ?>  
-  </ul>                      
+  </div>                      
   </div><!-- end:product_categories -->   
   
-  <div id="product_search">
-    <h3>产品搜索</h3>
-    <form id="search_form" method="get"><input id="keywords" type="text" name="keywords" /><button type="submit">搜</button></form>
-  </div> 
-  <script type="text/javascript">
-    $(function(){
-        $("#search_form").submit(function(){    
-            window.location.href="products/search/" + $("#keywords").val();
-            return false;
-        });
-    });
-  </script>
+
 </div>                
 
 <div id="main">
