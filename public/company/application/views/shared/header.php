1b139696da85b000ed95c4ab5d0f48c16ce29e0c
qichunren <whyruby@gmail.com>
2011-05-08 21:36:55 +0800

    更新了layout,header,footer

--- public/company/application/views/shared/header.php
+++ public/company/application/views/shared/header.php
@@ -3,7 +3,7 @@
 <div id="nav">
   <ul class="menu">
     <li><a href="<?php echo base_url(); ?>">首页</a></li>  
-    <li><a href="<?php echo site_url('pages/about_us'); ?>">关于我们</a></li>
+    <li><a href="<?php echo site_url('pages/introduce'); ?>">集团简介</a></li>
     <li><a href="<?php echo site_url('products'); ?>">产品中心</a></li> 
     <li><a href="<?php echo site_url('news'); ?>">新闻中心</a></li>
     <li><a href="<?php echo site_url('pages/contact_us'); ?>">联系我们</a></li>
