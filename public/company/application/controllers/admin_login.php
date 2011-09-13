4ac42dc13167e18ec5806cbe300f01afb0e4ab8e
Qichunren 曹金华 <whyruby@gmail.com>
2011-09-13 08:32:54 -0400

    rename Admin_Login to admin_login, so we use index.php/admin_login for login page.

--- public/company/application/controllers/admin_login.php
+++ public/company/application/controllers/admin_login.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admin_Login extends MY_Controller {
+class admin_Login extends MY_Controller {
   
     function __construct()
   	{
