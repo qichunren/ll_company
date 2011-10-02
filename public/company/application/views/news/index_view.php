5205363bea439f2a5826d9721e5d11fe2f3f4a70
Qichunren 曹金华 <whyruby@gmail.com>
2011-10-02 21:40:00 +0800

    fixed layouts.

--- public/company/application/views/news/index_view.php
+++ public/company/application/views/news/index_view.php
@@ -1,7 +1,9 @@
-<div id="sidebar">  
-    
-      <?php echo anchor("/news/industry", "行业新闻"); ?>  
-      
+<div class="row">
+
+<div class="span4">
+
+      <?php echo anchor("/news/industry", "行业新闻"); ?>
+
       <div id="contact_info">
           地 址： 上海金沙江路1340弄2号<br />
           电 话： 021-52652192<br />
@@ -10,9 +12,10 @@
           手 机： 13901610575 <br />
           邮 箱： jczlsb@yahoo.cn <br />
       </div>
-      
-</div>
-<div id="main">
+
+</div><!-- end span4 -->
+
+<div class="span10">
     <h2 class="page_title">公司新闻</h2>
     <div>
         <ul>
@@ -20,11 +23,12 @@
             <li class="news">
               <div>
                 <span><?php echo $news_item->created_at; ?></span>
-                <?php echo anchor("/news/show/".$news_item->id, strip_tags($news_item->title)); ?>      
+                <?php echo anchor("/news/show/".$news_item->id, strip_tags($news_item->title)); ?>
               </div>
             </li>
             <?php endforeach; ?>
         </ul>
     </div>
-    <?php echo $this->pagination->create_links(); ?> 
-</div>
+    <?php echo $this->pagination->create_links(); ?>
+</div><!-- end span10 -->
+</div><!-- end: row -->
--- public/company/application/views/news/industry_view.php
+++ public/company/application/views/news/industry_view.php
@@ -1,9 +1,10 @@
-<div id="sidebar">  
-    
+<div class="row">
+<div id="sidebar" class="span4">
+
       <?php echo anchor("/news", "公司新闻"); ?>
-      
+
 </div>
-<div id="main">
+<div id="main" class="span10">
     <h2>行业新闻</h2>
     <div>
         <ul>
@@ -11,11 +12,12 @@
             <li class="news">
               <div>
                 <span><?php echo $news_item->created_at; ?></span>
-                <?php echo anchor("/news/show/".$news_item->id, $news_item->title); ?>      
+                <?php echo anchor("/news/show/".$news_item->id, $news_item->title); ?>
               </div>
             </li>
             <?php endforeach; ?>
         </ul>
     </div>
-    <?php echo $this->pagination->create_links(); ?> 
+    <?php echo $this->pagination->create_links(); ?>
+</div>
 </div>
