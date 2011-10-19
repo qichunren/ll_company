665dcb0ddd23e0cec62db68854025b848e43a19e
Qichunren 曹金华 <whyruby@gmail.com>
2011-10-19 14:42:40 +0800

    page item style.

--- public/company/application/views/pages/page_items_part.php
+++ public/company/application/views/pages/page_items_part.php
@@ -1,4 +1,4 @@
-<li><?php echo anchor("/pages/introduce", "集团简介"); ?></li> 
-<li><?php echo anchor("/pages/about_us", "关于我们"); ?></li> 
-<li><?php echo anchor("/pages/contact_us", "联系我们"); ?></li>
-<li><?php echo anchor("/pages/jobs", "加入我们"); ?></li>
+<li><?php echo anchor("/pages/introduce", "集团简介", "class='btn'"); ?></li>
+<li><?php echo anchor("/pages/about_us", "关于我们", "class='btn'"); ?></li>
+<li><?php echo anchor("/pages/contact_us", "联系我们", "class='btn'"); ?></li>
+<li><?php echo anchor("/pages/jobs", "加入我们", "class='btn'"); ?></li>
