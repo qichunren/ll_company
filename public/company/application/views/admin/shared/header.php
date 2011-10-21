e1d4f497074df3f1e947ceb50024f212720b8838
Qichunren 曹金华 <whyruby@gmail.com>
2011-10-21 18:08:45 +0800

    refract admin layout.

--- public/company/application/views/admin/shared/header.php
+++ public/company/application/views/admin/shared/header.php
@@ -1,26 +1,10 @@
 <div id="header">
-    <div style="float:right;"><a href="<?php echo base_url(); ?>" target="_blank">查看网站</a></div><h1>上海佳诚工程设备有限公司 后台管理</h1>
+    <div style="float:right;">
+       <a href="<?php echo base_url(); ?>" target="_blank">查看网站</a>
+    </div>
+    <h1>上海佳诚工程设备有限公司 后台管理</h1>
     <div id="right_btn">
       欢迎您，<?php echo $this->session->userdata('admin_true_name'); ?>
       <a href="<?php echo site_url('admin_login/out'); ?>">退出</a>
-    </div>  
-</div>
-
-<div id="sidebar">
-    <div id="nav">
-      <ul class="menu">
-        <li><a <?php if($this->uri->segment(2) == 'dashboard'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/dashboard'); ?>">首页</a></li>
-        <li><a <?php if($this->uri->segment(2) == 'news'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/news'); ?>">新闻中心</a></li>
-        <li><a <?php if($this->uri->segment(2) == 'products'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/products'); ?>">产品中心</a>
-            <?php if($this->uri->segment(2) == 'products'){ ?>
-                <ul class="sub_menu">
-                    <li><a class="sub_menu_a" href="<?php echo site_url('/admin/products/add'); ?>">发布产品</a></li>
-                    <li><a class="sub_menu_a" href="<?php echo site_url('/admin/products/categories'); ?>">产品分类</a></li>
-                </ul>
-            <?php } ?>
-            </li>
-        <li><a <?php if($this->uri->segment(2) == 'setting'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/setting'); ?>">站点设置</a></li>
-        <li><a <?php if($this->uri->segment(2) == 'admins'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/admins'); ?>">帐号管理</a></li> 
-      </ul>
-    </div>          
-</div>
+    </div>
+</div><!-- end:header -->
--- public/company/application/views/layouts/admin.php
+++ public/company/application/views/layouts/admin.php
@@ -3,25 +3,36 @@
   <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <title>上海佳诚工程设备有限公司 后台管理</title>
-    <meta name="Keywords" content="" />
-    <meta name="Description" content="" />
     <link href="<?php echo base_url(); ?>assets/stylesheets/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css" />
-
     <link href="<?php echo base_url(); ?>assets/stylesheets/admin.css" media="screen" rel="stylesheet" type="text/css" />
     <script src="<?php echo base_url(); ?>assets/javascripts/jquery.js" type="text/javascript"></script>
     <script src="<?php echo base_url(); ?>assets/javascripts/admin.js?v=2" type="text/javascript"></script>
   </head>
   <body>
-    <div id="wrapper">
-      <?php $this->load->view("/admin/shared/header"); ?>
-
-      <div id="content">
+    <?php $this->load->view("/admin/shared/header"); ?>
+    <div class="container-fluid">
+       <div class="sidebar">
+           <div id="nav">
+             <ul class="menu">
+               <li><a <?php if($this->uri->segment(2) == 'dashboard'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/dashboard'); ?>">首页</a></li>
+               <li><a <?php if($this->uri->segment(2) == 'news'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/news'); ?>">新闻中心</a></li>
+               <li><a <?php if($this->uri->segment(2) == 'products'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/products'); ?>">产品中心</a>
+                   <?php if($this->uri->segment(2) == 'products'){ ?>
+                       <ul class="sub_menu">
+                           <li><a class="sub_menu_a" href="<?php echo site_url('/admin/products/add'); ?>">发布产品</a></li>
+                           <li><a class="sub_menu_a" href="<?php echo site_url('/admin/products/categories'); ?>">产品分类</a></li>
+                       </ul>
+                   <?php } ?>
+                   </li>
+               <li><a <?php if($this->uri->segment(2) == 'setting'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/setting'); ?>">站点设置</a></li>
+               <li><a <?php if($this->uri->segment(2) == 'admins'){ echo "class='current_tab'";} ?> href="<?php echo site_url('admin/admins'); ?>">帐号管理</a></li>
+             </ul>
+           </div>
+       </div><!-- end sidebar -->
+      <div class="content">
         <?php $this->load->view($view); ?>
-      </div>
-
-      <div class="clear"></div>
-
-      <?php $this->load->view("/admin/shared/footer"); ?>
-    </div>
+      </div><!-- end content -->
+    </div><!-- end container-fluid -->
+    <?php $this->load->view("/admin/shared/footer"); ?>
   </body>
 </html>
