1507462b5a0e9162ec1195a411924f7051a835dc
Qichunren 曹金华 <whyruby@gmail.com>
2012-02-03 14:01:10 +0800

    Upgrading from 2.0.1 to 2.0.2

--- public/company/system/core/CodeIgniter.php
+++ public/company/system/core/CodeIgniter.php
@@ -32,7 +32,7 @@
  *  Define the CodeIgniter Version
  * ------------------------------------------------------
  */
-	define('CI_VERSION', '2.0.1');
+	define('CI_VERSION', '2.0.2');
 
 /*
  * ------------------------------------------------------
@@ -53,7 +53,14 @@
  *  Load the framework constants
  * ------------------------------------------------------
  */
-	require(APPPATH.'config/constants'.EXT);
+	if (defined('ENVIRONMENT') AND file_exists(APPPATH.'config/'.ENVIRONMENT.'/constants'.EXT))
+	{
+		require(APPPATH.'config/'.ENVIRONMENT.'/constants'.EXT);
+	}
+	else
+	{
+		require(APPPATH.'config/constants'.EXT);
+	}
 
 /*
  * ------------------------------------------------------
@@ -188,6 +195,13 @@
 			exit;
 		}
 	}
+
+/*
+ * -----------------------------------------------------
+ * Load the security class for xss and csrf support
+ * -----------------------------------------------------
+ */
+	$SEC =& load_class('Security', 'core');
 
 /*
  * ------------------------------------------------------
